e3262c3e5b71ca32d8caf6905ce74e2f6436a5d7
John-Hephaistos <152403268+John-Hephaistos@users.noreply.github.com>
2024-05-20 12:35:48 +0200

    Update nn_model.py - Now in pytorch

--- nn_model.py
+++ nn_model.py
@@ -1,69 +1,90 @@
-import tensorflow as tf
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
 import sklearn
 from sklearn.model_selection import train_test_split
-from keras import models, layers
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 
-NR_OF_NOTES = 16
-NR_OF_EPOCHS = 50
-BATCH_SIZE = 20
+class MLP(nn.Module):
+    def __init__(self, input_size, output_size):
+        super(MLP, self).__init__()
+        self._layers = nn.Sequential(
+            torch.nn.Linear(input_size, 128),
+            torch.nn.ReLU(),
+            torch.nn.Linear(128, 256),
+            torch.nn.Sigmoid(),
+            torch.nn.Linear(256, output_size)
+        )
+        self.double()
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=0.01)
+        self.loss_function = torch.nn.MSELoss()
 
-data = np.load('dataset.npy')
-#print(np.shape(data))
-train_data, test_data = train_test_split(data, test_size=0.2, train_size=0.8, shuffle=True)
-
-
-
-train_x_data, train_y_data = [], []
-
-for samples in train_data:
-    train_x_data.append(samples[0])
-    train_y_data.append(samples[1])
-#print(np.shape(train_x_data))
-
-#print(np.shape(test_data))
-test_x_data, test_y_data = [], []
-for samples in test_data:
-    test_x_data.append(samples[0])
-    test_y_data.append(samples[1])
-#print(np.shape(test_x_data))
-
-def create_model(input_shape):
-    model = models.Sequential()
-    model.add(layers.Input(shape=input_shape))
-    model.add(layers.Flatten())
-    model.add(layers.Dense(120, activation='relu'))
-    model.add(layers.Dense(85, activation='relu'))
-    model.add(layers.Dense(np.prod(input_shape), activation='sigmoid'))
-    model.add(layers.Reshape(input_shape))
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-    model.fit(train_x_data, train_y_data, epochs=100, batch_size = 1, shuffle=True, verbose=1)
-    model.summary()
-    return model
+    def train_model(self, epochs, train_x_data, train_y_data):
 
 
-def calculate_accuracy(model, test_x, test_y):
-    total = 0
-    correct = 0
-    for (x, y) in zip(test_x, test_y):
-        total += 1
-        prediction = model.predict(x)
-        prediction = (prediction > 0.5).astype(int)
-        if prediction == test_y:
-            correct += 1
-    return correct / total
+        for epoch in range(epochs):
+            for X, y in zip(train_x_data, train_y_data):
+                X = torch.tensor(X, dtype=torch.double)  # Ensure input is of type double
+                y = torch.tensor(y, dtype=torch.double)
+                output = self.forward(X)
+                loss = self.loss_function(output, y)
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+                print(f"Epoch: {epoch + 1}; Loss: {loss.item()}")
+
+    def test_model(self, test_x_data, test_y_data):
+        total_steps = len(test_x_data)
+        correct = 0
+        for X, y in zip(test_x_data, test_y_data):
+            X = torch.tensor(X, dtype=torch.double)  # Ensure input is of type double
+            y = torch.tensor(y, dtype=torch.int)
+            prediction = (self.forward(torch.tensor(X, dtype=torch.double))).to(torch.int)
+            if torch.equal(prediction, y):
+                correct += 1
+        print(correct / total_steps)
+
+
+    def forward(self, model_input):
+        return self._layers(model_input)
+
 
 
 def main():
-    print(np.shape(train_x_data))
-    input_shape = (4, 4)
-    model = create_model(input_shape)
-    accuracy = calculate_accuracy(model, test_x_data, test_y_data)
-    print(accuracy)
 
+
+    data = np.load('dataset.npy')
+
+    train_data, test_data = train_test_split(data, test_size=0.2, train_size=0.8, shuffle=True)
+
+    train_x_data, train_y_data = [], []
+
+    for samples in train_data:
+        train_x_data.append(samples[0].flatten())
+        train_y_data.append(samples[1].flatten())
+    # print(np.shape(train_x_data))
+
+    # print(np.shape(test_data))
+    test_x_data, test_y_data = [], []
+    for samples in test_data:
+        test_x_data.append(samples[0].flatten())
+        test_y_data.append(samples[1].flatten())
+
+    #train_x_data = tuple(test_x_data)
+
+
+    print((test_x_data[1]))
+
+
+
+    model = MLP(16, 16)
+    print(model)
+    model.train()
+    model.train_model(10, train_x_data, train_y_data)
+    model.test_model(test_x_data, test_y_data)
+    #prediction = (model.forward(torch.tensor(test_x_data[1], dtype=torch.double))).to(torch.int)
+    #print(prediction)
 
 if __name__ == '__main__':
     main()
-
